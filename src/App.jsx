9f3b7b107e18b54f89dd9748a463f7b58572c950
picev0 <ecotech3690s@yahoo.co.jp>
2022-06-27 21:58:26 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  const onClickButton = () => alert();
+  return (
+    <>
+      <h1>こんにちは!</h1>
+      <p>お元気ですか?</p>
+      <button onClick={}>ボタン</button>
+    </>
+  );
+};
+
+export default App;
